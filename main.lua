9b752cd4918ffbea64d3041589f7017d0575917f
Joseph R Miles <joe@jrm.dev>
2020-08-01 13:53:55 -0700

    Refactored code to use new found object syntax

--- main.lua
+++ main.lua
@@ -2,27 +2,33 @@
 function love.load()
     love.window.setTitle("🏓PONG")
 
-    window_width = love.graphics.getWidth()
-    window_height = love.graphics.getHeight()
+    window = {
+        width = love.graphics.getWidth(),
+        height = love.graphics.getHeight(),
+    }
 
-    ball_speed = 40
-    ball_x = window_width / 2
-    ball_y = window_height / 2
+    ball = {
+        speed = 40,
+        x = window.width / 2,
+        y = window.height / 2,
+    }
 
-    player_width = 100
-    player_x = love.mouse.getX() - player_width / 2
-    player_y = window_height - 40
+    player = {
+        width = 100,
+        x = love.mouse.getX() - player.width / 2,
+        y = window.height - 40,
+    }
 end
 
 function love.draw()
-    love.graphics.circle("fill", ball_x, ball_y, 5)
-    love.graphics.rectangle("fill", player_x, player_y, 100, 20)
+    love.graphics.circle("fill", ball.x, ball.y, 5)
+    love.graphics.rectangle("fill", player.x, player.y, 100, 20)
 end
 
 function love.update(dt)
-    ball_x = ball_x + (ball_speed * dt)
-    ball_y = ball_y + (ball_speed * dt)
+    ball.x = ball.x + (ball.speed * dt)
+    ball.y = ball.y + (ball.speed * dt)
 
-    player_x = love.mouse.getX() - player_width / 2
-    player_y = window_height - 40
+    player.x = love.mouse.getX() - player.width / 2
+    player.y = window.height - 40
 end
