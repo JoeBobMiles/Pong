a779028d1b3f279042c664815e65584c4fedab03
Joseph R Miles <joe@jrm.dev>
2020-08-01 13:28:58 -0700

    Have a moving ball now

--- main.lua
+++ main.lua
@@ -1,3 +1,17 @@
+
+function love.load()
+    love.window.setTitle("🏓PONG")
+
+    speed = 40
+    x = 0
+    y = 0
+end
+
 function love.draw()
-    love.graphics.print("Hello World!", 400, 300)
+    love.graphics.circle("fill", x, y, 5)
+end
+
+function love.update(dt)
+    x = x + (speed * dt)
+    y = y + (speed * dt)
 end
