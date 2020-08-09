fb2e09e683174d2853b87b9ff747fb675f517d41
Joseph R Miles <joe@jrm.dev>
2020-08-09 13:07:54 -0700

    Fixed incorrect wall offsets

--- main.lua
+++ main.lua
@@ -119,12 +119,12 @@ function Wall:new(wall, world, x1, y1, x2, y2)
     then
         error("World cannot be nil!", 2)
     else
-        wall.body = love.physics.newBody(world, x1, y2, "static")
+        wall.body = love.physics.newBody(world, x1, y1, "static")
         -- Shape coordinates are relative to body coordinates, so we have to
         -- transform the coordinates passed (absolute, world coordinates) into
         -- coordinates relative to the body position.
         wall.shape = love.physics.newEdgeShape(
-            x1 - x1, y1 - y1,
+            0, 0,
             x2 - x1, y2 - y1)
         wall.fixture = love.physics.newFixture(
             wall.body,
