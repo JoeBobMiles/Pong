8238d3edf8e19432b7ba8176d666925b91d3b883
Joseph R Miles <joe@jrm.dev>
2020-08-09 14:00:46 -0700

    Pulled out Player object into module

---
+++ core/object/Player.lua
@@ -0,0 +1,34 @@
+local GameObject = require("core.object.GameObject")
+
+-- Player class
+local Player = GameObject:new()
+
+function Player:new(player, world, x, y)
+    player = player or GameObject:new()
+
+    setmetatable(player, self)
+    self.__index = self
+
+    player.body = love.physics.newBody(world, x, y, "kinematic")
+    player.shape = love.physics.newRectangleShape(100, 10)
+    player.fixture = love.physics.newFixture(
+        player.body,
+        player.shape,
+        1)
+
+    return player
+end
+
+function Player:update()
+    game.objects.player.body:setX(love.mouse.getX())
+end
+
+function Player:draw()
+    love.graphics.setColor(1, 1, 1)
+    love.graphics.polygon(
+        "fill",
+        self.body:getWorldPoints(
+            self.shape:getPoints()))
+end
+
+return Player
--- main.lua
+++ main.lua
@@ -2,38 +2,8 @@ local Game = require("core.Game")
 local GameObject = require("core.object.GameObject")
 
 local Ball = require("core.object.Ball")
+local Player = require("core.object.Player")
 
-
--- Player class
-local Player = GameObject:new()
-
-function Player:new(player, world, x, y)
-    player = player or GameObject:new()
-
-    setmetatable(player, self)
-    self.__index = self
-
-    player.body = love.physics.newBody(world, x, y, "kinematic")
-    player.shape = love.physics.newRectangleShape(100, 10)
-    player.fixture = love.physics.newFixture(
-        player.body,
-        player.shape,
-        1)
-
-    return player
-end
-
-function Player:update()
-    game.objects.player.body:setX(love.mouse.getX())
-end
-
-function Player:draw()
-    love.graphics.setColor(1, 1, 1)
-    love.graphics.polygon(
-        "fill",
-        self.body:getWorldPoints(
-            self.shape:getPoints()))
-end
 
 -- (Invisible) Wall class
 local Wall = GameObject:new()
