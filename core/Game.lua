49d7c214f9cc5878a1a018d066d7b8b43cd7301b
Joseph R Miles <joe@jrm.dev>
2020-08-09 14:38:50 -0700

    Moved most game logic into Game object

--- core/Game.lua
+++ core/Game.lua
@@ -1,7 +1,14 @@
+local Ball = require("core.object.Ball")
+local Player = require("core.object.Player")
+local Wall = require("core.object.Wall")
+local Goal = require("core.object.Goal")
+
+
 -- Game class
 local Game = {
     world = nil,
     objects = nil,
+    isPaused = false,
 }
 
 function Game:new(game)
@@ -11,6 +18,73 @@ function Game:new(game)
     self.__index = self
 
     return game
+end
+
+function Game:init()
+    self.world = love.physics.newWorld(0, 0, true)
+    self.world:setCallbacks(beginContact, nil, nil, nil)
+
+    local window = {
+        width = love.graphics.getWidth(),
+        height = love.graphics.getHeight(),
+    }
+
+    self.objects = {}
+
+    self.objects.ball = Ball:new(
+        nil,
+        self.world,
+        window.width / 2, window.height / 2,
+        5)
+
+    local initialForce = 12800
+    self.objects.ball.body:applyForce(initialForce, initialForce)
+
+    self.objects.player = Player:new(
+        nil,
+        self.world,
+        0, window.height - 40)
+
+    self.objects.right_wall = Wall:new(
+        nil,
+        self.world,
+        window.width, 0,
+        window.width, window.height)
+    self.objects.left_wall = Wall:new(
+        nil,
+        self.world,
+        0, 0,
+        0, window.height)
+    self.objects.top_wall = Wall:new(
+        nil,
+        self.world,
+        0, 0,
+        window.width, 0)
+    self.objects.bottom_wall = Goal:new(
+        nil,
+        self.world,
+        0, window.height,
+        window.width, window.height)
+
+    self.isPaused = true
+end
+
+function Game:update(dt)
+    if self.isPaused then return end
+
+    self.world:update(dt)
+
+    for name, object in pairs(self.objects)
+    do
+        object:update(dt)
+    end
+end
+
+function Game:draw()
+    for name, object in pairs(self.objects)
+    do
+        object:draw()
+    end
 end
 
 return Game
--- main.lua
+++ main.lua
@@ -1,63 +1,12 @@
 local Game = require("core.Game")
 
-local Ball = require("core.object.Ball")
-local Player = require("core.object.Player")
-local Wall = require("core.object.Wall")
-local Goal = require("core.object.Goal")
-
 
 -- Game code
 function love.load()
     love.window.setTitle("🏓PONG")
 
     game = Game:new()
-
-    game.world = love.physics.newWorld(0, 0, true)
-    game.world:setCallbacks(beginContact, nil, nil, nil)
-
-    local window = {
-        width = love.graphics.getWidth(),
-        height = love.graphics.getHeight(),
-    }
-
-    game.objects = {}
-
-    game.objects.ball = Ball:new(
-        nil,
-        game.world,
-        window.width / 2, window.height / 2,
-        5)
-
-    local initialForce = 12800
-    game.objects.ball.body:applyForce(initialForce, initialForce)
-
-    game.objects.player = Player:new(
-        nil,
-        game.world,
-        0, window.height - 40)
-
-    game.objects.right_wall = Wall:new(
-        nil,
-        game.world,
-        window.width, 0,
-        window.width, window.height)
-    game.objects.left_wall = Wall:new(
-        nil,
-        game.world,
-        0, 0,
-        0, window.height)
-    game.objects.top_wall = Wall:new(
-        nil,
-        game.world,
-        0, 0,
-        window.width, 0)
-    game.objects.bottom_wall = Goal:new(
-        nil,
-        game.world,
-        0, window.height,
-        window.width, window.height)
-
-    isPaused = false
+    game:init()
 end
 
 function love.keyreleased(key, scancode)
@@ -71,21 +20,11 @@ function love.keyreleased(key, scancode)
 end
 
 function love.update(dt)
-    if isPaused then return end
-
-    game.world:update(dt)
-
-    for name, object in pairs(game.objects)
-    do
-        object:update()
-    end
+    game:update(dt)
 end
 
 function love.draw()
-    for name, object in pairs(game.objects)
-    do
-        object:draw()
-    end
+    game:draw()
 end
 
 -- World Collision Callbacks
