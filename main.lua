e075c871be122194ee52fef7523d14ff6c53390f
Joseph R Miles <joe@jrm.dev>
2020-08-09 13:23:12 -0700

    Pulled out GameObject class into module

--- main.lua
+++ main.lua
@@ -1,32 +1,6 @@
 local Game = require("core.Game")
+local GameObject = require("core.object.GameObject")
 
--- GameObject class
-local GameObject = {
-    body = nil,
-    shape = nil,
-    fixture = nil,
-}
-
-function GameObject:new(object, body, shape, fixture)
-    object = object or {}
-
-    setmetatable(object, self)
-    self.__index = self
-
-    body = body or nil
-    shape  = shape or nil
-    fixture = fixture or nil
-
-    return object
-end
-
-function GameObject:update()
-    error("GameObject:update() is not implemented", 2)
-end
-
-function GameObject:draw(dt)
-    error("GameObject:draw() is not implemented", 2)
-end
 
 -- Ball class
 local Ball = GameObject:new()
